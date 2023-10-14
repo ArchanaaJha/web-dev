39a4b8f4bcc232085bb276b919331ec331414912
Archana Jha <114074480+ArchanaaJha@users.noreply.github.com>
2023-10-14 13:36:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -16,4 +16,28 @@ function displayRelated(type) {
                                      </ul>`;
     }
   }
+  //   Level 2 - Generate Certificate for Teacher and Student Pair
+//  For simplicity, let's generate a certificate with the teacher and student names:
+function generateCertificate() {
+    const teacher = document.getElementById('teacher-select').value;
+    const student = document.getElementById('student-select').value;
+  
+    const certificateText = `Certificate of Achievement
+      This is to certify that ${student} has successfully completed the course under the guidance of ${teacher}.`;
+  
+    alert(certificateText);
+  }
+//   You can call this function when you want to generate a certificate.
+// Level 3 - Verify Certificate using JWT Token (Basic Example)
+function verifyCertificate() {
+    const certificateToken = 'example_jwt_token'; // Replace with your actual JWT token
+  
+    // Here you would verify the token using your backend server
+  
+    if (certificateToken) {
+      alert('Certificate verified successfully.');
+    } else {
+      alert('Certificate verification failed.');
+    }
+  }
   
